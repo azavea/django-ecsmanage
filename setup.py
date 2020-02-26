f335c0422864e298048c7e11ee7b777101ae15e8
Cole Kettler <me@colekettler.com>
2020-02-26 16:59:06 -0500

    Move setup.py configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,46 +1,7 @@
 import os
-from setuptools import find_packages, setup
-
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
-
-description = (
-    "Run any Django management command on an AWS Elastic Container Service"
-    "(ECS) cluster."
-)
+from setuptools import setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(
-    name="django-ecsmanage",
-    use_scm_version=True,
-    packages=find_packages(),
-    include_package_data=True,
-    license="Apache License 2.0",
-    description=description,
-    long_description=README,
-    url="https://github.com/azavea/django-ecsmanage/",
-    author="Azavea, Inc.",
-    author_email="systems@azavea.com",
-    install_requires=["Django>=1.11", "boto3>=1.9.0", "future-fstrings>=1.0.0"],
-    python_requires=">=3.5",
-    extras_require={"tests": ["flake8>=3.7.7", 'black;python_version>"3.6"']},
-    setup_requires=["setuptools_scm==3.*"],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-)
+setup(use_scm_version=True,)
