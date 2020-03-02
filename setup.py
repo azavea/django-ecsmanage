08b0bd72bbd212765960cd2e806ac2b00988979f
Cole Kettler <me@colekettler.com>
2020-03-02 11:18:01 -0500

    Remove working directory change from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-import os
 from setuptools import setup
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(use_scm_version=True,)
